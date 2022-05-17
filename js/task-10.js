77b7dbb0c7651604a2c5dd28fdf677f3eaa8bb34
Some14u <some1@email.ua>
2022-05-17 17:10:27 +0300

    Исправлен косяк в функциях генерации цвета

--- js/task-10.js
+++ js/task-10.js
@@ -16,7 +16,7 @@ function createBoxes(amount) {
   for(let i = 0; i < amount; i++) {
     const box = document.createElement("div");
     box.style.width = box.style.height = `${STARTER_SIZE + i * INCREMENT}px`;
-    box.style.backgroundColor = getRandomHexColor();
+    box.style.backgroundColor = box.textContent = getRandomHexColor();
     boxes.push(box);
   }
   boxesContainer.append(...boxes);
@@ -27,5 +27,5 @@ function destroyBoxes() {
 }
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
